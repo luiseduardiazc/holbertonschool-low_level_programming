5b0d677cb531b8cf4e56244aa65fbbbe02e2a1b6
luiseduardiazc <luis.diaz.car@gmail.com>
2019-06-20 18:54:58 +0000

    refactor code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,29 @@
-#include	<stdio.h>
-#include	<stdlib.h>
-#include	<time.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int	main(void)
+int main(void)
 {
-	int	n;
+	int n;
 
 	srand(time(0));
-	n	=	rand()	-	RAND_MAX	/	2;
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
 
-	if	(n	>	0)
-	{
-		printf("%s",	"is positive\n");
-	}
-	else	if	(n	==	0)
-	{
-		printf("%s",	"is zero\n");
-	}
-	else
-	{
-		printf("%s",	"is negative\n");
-	}
-
-	return	(0);
+	return (0);
 }
-
