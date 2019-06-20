9bb8c03af2c784b1c24b5a806ed30912729900a5
luiseduardiazc <luis.diaz.car@gmail.com>
2019-06-20 20:52:54 +0000

    dont print q and e letter

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		if (n != 113 && n != 101)
+		{
+			putchar(n);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
