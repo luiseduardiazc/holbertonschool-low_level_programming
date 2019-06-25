06ce1451297db881d3f666f31f2ec494b0b4f35e
luiseduardiazc <luis.diaz.car@gmail.com>
2019-06-25 02:29:12 +0000

    Refactor code

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,30 @@
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 #include "holberton.h"
-int main(void)
+/**
+ *print_alphabet_x10 - prints the alphabet, in lowercase
+ *
+ *Return: Nothing
+ */
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		print_alphabet();
+		 _putchar('\n');
+	}
+}
+/**
+ *print_alphabet - prints the letters from a to z in lowercase
+ *
+ *Return: 0 upon successful completion
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
 }
