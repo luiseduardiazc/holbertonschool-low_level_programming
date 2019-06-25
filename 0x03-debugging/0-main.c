7c11bd7a274399b173bc6aca01d25867d6e467d7
luiseduardiazc <luis.diaz.car@gmail.com>
2019-06-25 22:34:17 +0000

    add new indentation

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
@@ -6,9 +7,9 @@
 
 int main(void)
 {
-		int i;
+	int i;
 
-		i = 0;
-		positive_or_negative(i);
-		return (0);
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
