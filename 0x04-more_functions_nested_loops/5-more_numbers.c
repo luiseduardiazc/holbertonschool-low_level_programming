046e44b82da7dc6e482d4e0e33f5fe65464ce42a
luiseduardiazc <luis.diaz.car@gmail.com>
2019-06-27 19:02:57 +0000

    Refactor code for use only 3 _putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,22 +6,20 @@
  */
 void more_numbers(void)
 {
-	int i, j, first, last;
+	int i, j, num;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j >= 10)
+			if (j > 9)
 			{
-				first = j / 10;
-				last = j % 10;
-				_putchar(first + '0');
-				_putchar(last + '0');
-			} else
-			{
-				_putchar(j + '0');
+				num = j / 10;
+				_putchar(num + '0');
 			}
+			num = j % 10;
+			_putchar(num + '0');
 		}
+		_putchar('\n');
 	}
 }
