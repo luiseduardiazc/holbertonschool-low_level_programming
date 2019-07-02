9dc10b69713bd19a09a00f61b7ac3dad7288a090
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-02 12:19:12 +0000

    print the second half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ *_strlen - reset number
+ *Description: This function return a length for some string
+ *@s: pointer char
+ *Return: int length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ *puts_half - check the code for Holberton School students.
+ *@s: pointer parameter
+ *Description: This function prints pair number
+ *Return: Always Nothing.
+ */
+void puts_half(char *s)
+{
+	int i;
+	int len = _strlen(s) / 2;
+
+	for (i = 0 ; i < _strlen(s); i++)
+	{
+		if (i >= len)
+		{
+			_putchar(s[i]);
+		}
+	}
+	_putchar('\n');
+}
