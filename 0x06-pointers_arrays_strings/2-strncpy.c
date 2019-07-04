28a3d96a47b366fa4460a26a97935e5e7ca5bb37
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-04 20:53:26 +0000

    function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ *_strlen - reset number
+ *Description: This function return a length for some string
+ *@s: pointer char
+ *Return: int length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ *_strncpy - similar function: strcat
+ *@dest: pointer parameter"
+ *@src: pointer parameter"
+ *@n: int parameter
+ *Description: This function merge two strings
+ *Return: pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
