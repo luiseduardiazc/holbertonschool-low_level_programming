3500196346086da1cd1d47bc8cde3b28dd2351dc
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-04 23:01:54 +0000

    Refactor Code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,48 +1,26 @@
 #include "holberton.h"
 /**
- *_strlen - reset number
- *Description: This function return a length for some string
- *@s: pointer char
- *Return: int length
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s++)
-	{
-		len++;
-	}
-	return (len);
-}
-/**
  *_strcmp - function that compares two strings
  *@s1: pointer parameter"
  *@s2: pointer parameter"
  *Description: This function compare two strings
- *Return: int
+ *Return: return int
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len_s1 = _strlen(s1);
-	int i, j = 0;
-
-	for (i = 0; i <= len_s1; i++)
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		if (s1[i] != s2[i])
-		{
-			if (s1[i] > s2[i])
-			{
-				j = 15;
-				break;
-			}
-			if (s1[i] < s2[i])
-			{
-				j = -15;
-				break;
-			}
-		}
+		s1++;
+		s2++;
 	}
-	return (j);
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
