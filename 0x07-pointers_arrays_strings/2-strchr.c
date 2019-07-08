cd92429a827333f4fc7da84fffc6ae5dce0b7e7d
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-08 18:45:36 +0000

    locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_strchr - locates a character in a string
+ *@s: pointer char parameter
+ *@c: char parameter
+ *Description: locates a character in a string
+ *Return: pointer
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (s);
+}
