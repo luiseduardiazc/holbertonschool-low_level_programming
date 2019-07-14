78f4ec68e2403498bbde857bd6c92fb06a724d0b
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-14 18:38:56 +0000

    Create a function isNumeric

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,20 +3,31 @@
 #include <string.h>
 #include <ctype.h>
 /**
+ *isNumeric - checks numbers
+ *@s: conts char pointer
+ *Description: This function checks for numbers
+ *Return: 1 for numbers 0 for alpha
+ */
+int isNumeric(const char *s)
+{
+	char *p = NULL;
+
+	if (s == NULL || *s == '\0' || isspace(*s))
+		return (0);
+	strtod (s, &p);
+	return (*p == '\0');
+}
+/**
  * main - program that adds positive numbers
  * @argc: argument count
  * @argv: numbers to add
+ * Description: this is the main function
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	int i, val, sum = 0;
 
-	if (argc == 2 && !isdigit(*argv[1]))
-	{
-		printf("%s\n", "Error");
-		return (1);
-	}
 	if (argc < 3)
 	{
 		printf("%d\n", 0);
@@ -24,7 +35,8 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (!isdigit(*argv[i]))
+			char *value = argv[i];
+			if (!isNumeric(value))
 			{
 				printf("%s\n", "Error");
 				return (1);
