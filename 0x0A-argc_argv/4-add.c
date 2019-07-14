e7e47d05ec98066a141d0602a584e441713d8483
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-14 15:21:35 +0000

    Refactor code and test new solution

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 /**
  * main - program that adds positive numbers
  * @argc: argument count
  * @argv: numbers to add
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int sum;
+	int i, val, sum = 0;
 
 	if (argc == 1)
 	{
 		printf("%d\n", 0);
 		return (0);
 	}
-	while (argc-- && argc > 0)
+	for (i = 1; i < argc; i++)
 	{
-		argv++;
-		if (!atoi(*argv) || atoi(*argv) < 0)
+		if (!isdigit(*argv[i]))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(*argv);
+	val = atoi(argv[i]);
+	sum += val;
 	}
 	printf("%d\n", sum);
 	return (0);
