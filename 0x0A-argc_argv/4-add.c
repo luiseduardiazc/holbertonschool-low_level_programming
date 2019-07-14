80401e728148e2d6c8a3110830da92363b2d36b0
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-14 07:29:22 +0000

    Refactor code

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,19 +11,15 @@ int main(int argc, char **argv)
 {
 	int sum;
 
-	if (argc < 1)
+	if (argc == 1)
 	{
 		printf("%d\n", 0);
+		return (0);
 	}
-
 	while (argc-- && argc > 0)
 	{
-		if (!atoi(*++argv))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		if (atoi(*argv) < 0)
+		argv++;
+		if (!atoi(*argv) || atoi(*argv) < 0)
 		{
 			printf("Error\n");
 			return (1);
