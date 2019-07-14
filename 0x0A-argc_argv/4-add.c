d1dd50de260b9548e52cafe30417d3eca0ce1d56
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-14 22:02:18 +0000

    crazy solution

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,11 +12,12 @@ int main(int argc, char *argv[])
 {
 	int sum = 0, i = 1;
 
-	if (argc < 3)
-	{
-		printf("%d\n", 0);
-		return (1);
-	}
+	/**
+	  if (argc < 3)
+	  {
+	  printf("%d\n", 0);
+	  return (1);
+	  } */
 	while (i < argc)
 	{
 		if (!isdigit(*argv[i]))
@@ -28,5 +29,5 @@ int main(int argc, char *argv[])
 		i++;
 	}
 	printf("%d\n", sum);
-	return (0);
+	return (sum);
 }
