338ddc782d82899e1129179065735f565c17bf1d
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-16 18:39:47 +0000

    function that returns a pointer to a newly allocated

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *_strdup - copy pointer
+ *@str: pointer char parmeter
+ *Description: function for copy pointer
+ *Return: pointer
+ */
+char *_strdup(char *str)
+{
+	char *ar;
+	int i = 0;
+
+	ar = malloc(strlen(str) * sizeof(char));
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
+	while (*str != '\0')
+	{
+		ar[i] = *str;
+		str++;
+		i++;
+	}
+	return (ar);
+}
