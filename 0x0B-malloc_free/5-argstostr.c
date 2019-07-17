14def33d262ea205d3b0496ebabcf786ebaeef11
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-17 05:04:46 +0000

    Refactor code and test new solution

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,42 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *_strlen - reset number
- *Description: This function return a length for some string
- *@s: pointer char
- *Return: int length
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s++)
-	{
-		len++;
-	}
-	return (len);
-}
-/**
- *_strcat - similar function: strcat
- *@dest: pointer parameter whit string "Hello "
- *@src: pointer parameter whit string "World!\n"
- *Description: This function merge two strings
- *Return: "Hello World!"
- */
-char *_strcat(char *dest, char *src)
-{
-	int dest_len = _strlen(dest);
-	int n = _strlen(src);
-	int i;
-
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
-		dest[dest_len + i] = src[i];
-	dest[dest_len + i] = '\0';
-
-	return (dest);
-}
-/**
  *argstostr - concatenate
  *@ac: int parameter
  *@av: double pointer parameter
@@ -45,31 +9,43 @@ char *_strcat(char *dest, char *src)
  */
 char *argstostr(int ac, char **av)
 {
-	char *s;
-	int i, j, a;
-	int cont = 0;
-	char enter[] = {'\n'};
+	int i, j, count = 0;
+	char *aux;
+	char *string;
 
 	if (ac == 0 || av == NULL)
 	{
 		return (NULL);
 	}
+
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j] != 0; j++)
 		{
-			cont++;
+			count++;
 		}
+		count++;
 	}
-	s = malloc(sizeof(char) * (cont * ac));
-	if (s == NULL)
+	count += 1;
+
+	aux = malloc(sizeof(char) * count);
+	string = aux;
+
+	if (aux == NULL)
 	{
 		return (NULL);
 	}
-	for (a = 0; a < ac; a++)
+
+	for (i = 0; i < ac; i++)
 	{
-		_strcat(s, av[a]);
-		_strcat(s, enter);
+		for (j = 0; av[i][j] != 0; j++)
+		{
+			*aux = av[i][j];
+			aux++;
+		}
+		*aux = '\n';
+		aux++;
 	}
-	return (s);
+
+	return (string);
 }
