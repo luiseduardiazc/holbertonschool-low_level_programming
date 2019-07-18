751068c6a236133eb4fb8b50ac0eeb64dbfaab16
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-18 19:58:34 +0000

    avoid previus functions refactor code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
 /**
  *_strlen - reset number
  *Description: This function return a length for some string
@@ -9,51 +8,13 @@
 
 unsigned int _strlen(char *s)
 {
-	unsigned int len = 0;
+	int len = 0;
 
 	while (*s++)
 	{
 		len++;
 	}
 	return (len);
-}
-/**
- *_strncat - similar function: strcat
- *@dest: pointer parameter whit string "Hello "
- *@src: pointer parameter whit string "World!\n"
- *@n: int parameter
- *Description: This function merge n bytes from src
- *Return: pointer string
- */
-char *_strncat(char *dest, char *src, int n)
-{
-	int dest_len = _strlen(dest);
-	int i;
-
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
-		dest[dest_len + i] = src[i];
-	dest[dest_len + i] = '\0';
-	return (dest);
-}
-/**
- *_strcpy - reset number
- *Description: This function get an array and print n elements
- *@dest: pointer char
- *@src: int parameter
- *Return: pointer
- */
-char *_strcpy(char *dest, char *src)
-{
-	unsigned int i = 0;
-
-	while (i <= _strlen(src))
-	{
-		char temp = src[i];
-		*dest = temp;
-		dest++;
-		i++;
-	}
-	return (src);
 }
 /**
  *string_nconcat - concatenates two strings
@@ -66,26 +27,41 @@ char *_strcpy(char *dest, char *src)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	char *aux;
-	int size = 0;
-	unsigned int words = n;
+	unsigned int i, j;
 
-	if (words >= _strlen(s2))
+	if (s1 == NULL)
 	{
-		words = _strlen(s2);
+		s1 = "";
 	}
 
-	size = _strlen(s1) + words + 1;
-	str = malloc(sizeof(char) * size);
-	aux = str;
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	if ((int) n >= _strlen(s2))
+	{
+		n = _strlen(s2);
+	}
+
+	str = malloc(_strlen(s1) + n + 1);
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-
-	_strcpy(aux, s1);
-	_strncat(aux, s2, words);
+	/** copy s1 to str */
+	for (i = 0, j = 0; s1[i] != '\0'; i++, j++)
+	{
+		str[j] = s1[i];
+	}
+	/** concatenate s2 to str */
+	for (i = 0; i != n; i++, j++)
+	{
+		str[j] = s2[i];
+	}
+	/** end null caracter */
+	str[j] = '\0';
 
 	return (str);
 }
