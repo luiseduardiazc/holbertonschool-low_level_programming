3a1abd4ee61000afaf4e9399182bc85e0f90d6f3
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-18 23:27:41 +0000

    first commit

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+/**
+ *_strlen - reset number
+ *Description: This function return a length for some string
+ *@s: pointer char
+ *Return: int length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ *_strcpy - reset number
+ *Description: This function get an array and print n elements
+ *@dest: pointer char
+ *@src: int parameter
+ *Return: pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (i <= _strlen(src))
+	{
+		char temp = src[i];
+		*dest = temp;
+		dest++;
+		i++;
+	}
+	return (src);
+}
+/**
+ *_realloc - realloc
+ *@ptr: pointer
+ *@old_size: old_size memory
+ *@new_size: new_size memory
+ *Description: function realloc memory
+ *Return: pointer or NULL
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (old_size == new_size)
+	{
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ptr = malloc(new_size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	return (ptr);
+}
