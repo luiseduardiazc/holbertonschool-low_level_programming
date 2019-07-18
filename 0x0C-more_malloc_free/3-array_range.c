5b54962183d51220e45d33ba25c7d4367054bf50
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-18 22:08:41 +0000

    creates an array of integers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+/**
+ *_calloc - allowed memory
+ *@nmemb: unsigned int parameter
+ *@size: unsigned int parameter
+ *Description: function that allocates memory for array
+ *Return: pointer or NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = (char *)malloc(nmemb * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		ptr[i] = 0;
+	}
+
+	return (ptr);
+}
+/**
+ *array_range - array range
+ *@min: int parameter
+ *@max: int parameter
+ *Description: function that allocates memory for array range
+ *Return: pointer or NULL
+ */
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	ptr = _calloc(max, sizeof(int));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = min; i <= max; i++)
+	{
+		ptr[i] = i;
+	}
+
+	return (ptr);
+}
