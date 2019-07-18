96202a742d695e447cac4793beaca4475522e1f3
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-18 22:32:31 +0000

    fixed

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,36 +1,5 @@
 #include <stdlib.h>
 /**
- *_calloc - allowed memory
- *@nmemb: unsigned int parameter
- *@size: unsigned int parameter
- *Description: function that allocates memory for array
- *Return: pointer or NULL
- */
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	char *ptr;
-	unsigned int i;
-
-	if (nmemb == 0 || size == 0)
-	{
-		return (NULL);
-	}
-
-	ptr = (char *)malloc(nmemb * size);
-
-	if (ptr == NULL)
-	{
-		return (NULL);
-	}
-
-	for (i = 0; i < (nmemb * size); i++)
-	{
-		ptr[i] = 0;
-	}
-
-	return (ptr);
-}
-/**
  *array_range - array range
  *@min: int parameter
  *@max: int parameter
@@ -47,7 +16,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	}
 	cant = max - min;
-	ptr = _calloc(cant + 1, sizeof(int));
+	ptr = malloc((cant + 1) * sizeof(int));
 
 	if (ptr == NULL)
 	{
