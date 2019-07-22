78370d0774afc06d764f85814d6f6c09cbf0ed05
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-22 20:23:06 +0000

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_HOLBERTON
+#define HEADER_HOLBERTON
+/**
+ * struct dog - dog structure
+ * @name: char pointer member
+ * @age: float member
+ * @owner: char pointer member
+ *
+ * Description: This structure store data from dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
