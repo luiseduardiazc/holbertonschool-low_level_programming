a18e0295ee0fe3d904e5b91e8691dbeda9af6aa9
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-22 23:16:03 +0000

    Add valitation for null parameters

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,6 +13,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *d;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
 	d = malloc(sizeof(struct dog));
 
 	if (d == NULL)
