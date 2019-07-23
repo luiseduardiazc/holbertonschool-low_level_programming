f9cf844f2c4f22f46f486ff39372415ae86d5c0a
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-23 00:13:29 +0000

    Add new solution

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,44 @@
 #include <stdio.h>
 #include "dog.h"
 /**
+ *_strlen - reset number
+ *Description: This function return a length for some string
+ *@s: pointer char
+ *Return: int length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ *_strcpy - reset number
+ *Description: This function get an array and print n elements
+ *@dest: pointer char
+ *@src: int parameter
+ *Return: pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (i <= _strlen(src))
+	{
+		char temp = src[i];
+		*dest = temp;
+		dest++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (src);
+}
+/**
  *new_dog - creates a new dog
  *@name: char pointer
  *@age: float parameter
@@ -11,21 +49,39 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *d;
+	dog_t *new_dog;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
-	d = malloc(sizeof(struct dog));
-
-	if (d == NULL)
+	new_dog = malloc(sizeof(struct dog));
+	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
-
-	return (d);
+	if (name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (new_dog->name == NULL)
+	{
+		return (NULL);
+	}
+	_strcpy(new_dog->name, name);
+	new_dog->age = age;
+	if (owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
+		return (NULL);
+	}
+	_strcpy(new_dog->owner, owner);
+	return (new_dog);
 }
