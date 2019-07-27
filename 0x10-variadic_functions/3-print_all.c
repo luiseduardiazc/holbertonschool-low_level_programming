d54332a563269efbeeb7579c92d8f90dc3476310
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-27 19:01:12 +0000

    Fixed again

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -24,43 +24,44 @@ int _strlen(char *s)
  */
 void print_all(const char * const format, ...)
 {
-	va_list arguments;
-	int i = 0, flag = 0;
-	double _double;
-	char *s;
+	va_list anything;
+	int i = 0;
+	char *string;
 
+	va_start(anything, format);
 	while (format == NULL)
+	{
+		printf("\n");
 		return;
-	va_start(arguments, format);
+	}
 	while (format[i] != '\0')
 	{
-		flag = 0;
 		switch (format[i])
 		{
-
 			case 'c':
-				printf("%c", va_arg(arguments, int));
+				printf("%c", (char) va_arg(anything, int));
 				break;
 			case 'i':
-				printf("%d", va_arg(arguments, int));
+				printf("%d", va_arg(anything, int));
 				break;
 			case 'f':
-				_double = va_arg(arguments, double);
-				printf("%f", (float)_double);
+				printf("%f", (float) va_arg(anything, double));
 				break;
 			case 's':
-				s = va_arg(arguments, char *);
-				if (!s)
-					s = "(neil)";
-				printf("%s", s);
+				string = va_arg(anything, char *);
+				if (string != NULL)
+				{
+					printf("%s", string);
+					break;
+				}
+				printf("(nil)");
 				break;
-			default:
-				flag = 1;
 		}
-		if (flag == 0 && i != _strlen((char *)format) - 1)
+		if ((format[i] == 'c' || format[i] == 's' || format[i] == 'i'
+			|| format[i] == 'f') && (format[i + 1] != '\0'))
 			printf(", ");
 		i++;
 	}
+	va_end(anything);
 	printf("\n");
-	va_end(arguments);
 }
