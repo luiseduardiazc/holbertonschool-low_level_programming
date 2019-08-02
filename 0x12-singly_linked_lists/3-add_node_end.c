16f607702f6ce8f6e8ed3964913ab511da4f9823
luiseduardiazc <luis.diaz.car@gmail.com>
2019-08-02 04:23:00 +0000

    Add 3 task

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+/**
+ *_strlen - reset number
+ *Description: This function return a length for some string
+ *@s: pointer char
+ *Return: int length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ * add_node_end - add node end
+ * @head: pointer to pointer to list_t
+ * @str: conts char string
+ * Description: Add new node at the end of a list_t list
+ * Return: Return new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	list_t *current;
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = _strlen(new_node->str);
+	new_node->next = NULL;
+
+	if (!*head)
+		*head = new_node;
+	else
+	{
+		current = *head;
+		while (current->next)
+			current = current->next;
+		current->next = new_node;
+	}
+
+	return (new_node);
+}
