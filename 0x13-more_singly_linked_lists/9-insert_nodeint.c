22710522da09facd196a4e56d8fd4e824653f3ca
luiseduardiazc <luis.diaz.car@gmail.com>
2019-08-06 19:02:08 +0000

    add new solution

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,11 +36,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	unsigned int cont = 0;
 
-	if (*head == NULL)
-	{
-		return (NULL);
-	}
-
 	if (idx == 0)
 	{
 		return (add_nodeint(head, n));
@@ -66,5 +61,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		cont++;
 	}
 
-	return (NULL);
+	return (*head);
 }
