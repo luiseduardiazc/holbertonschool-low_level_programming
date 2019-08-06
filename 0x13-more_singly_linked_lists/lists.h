38bdd7ba01e6b78e6255a027765369b1f735ecf5
luiseduardiazc <luis.diaz.car@gmail.com>
2019-08-06 15:10:24 +0000

    remove string.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,7 +2,6 @@
 #define LINKED_LIST_LISTS_H
 #include <stdio.h>
 #include "stdlib.h"
-#include "string.h"
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -13,8 +12,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
