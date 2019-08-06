b1eac0e039541aecda46e88882ffb4f94a1ae557
luiseduardiazc <luis.diaz.car@gmail.com>
2019-08-06 15:27:42 +0000

    fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,14 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *node = *head;
+	listint_t *node;
 	listint_t *temp;
 
 	if (head == NULL)
 	{
 		return;
 	}
+	node = *head;
 	while (node != NULL)
 	{
 		temp = node;
