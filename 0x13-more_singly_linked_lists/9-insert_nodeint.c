42504d5242875f2e76c16a730c017396bd7dbd54
luiseduardiazc <luis.diaz.car@gmail.com>
2019-08-06 18:42:32 +0000

    manage case idx = 0 insert at begining

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,27 @@
 #include "lists.h"
 /**
+ * add_nodeint - add node int
+ * @head: pointer to pointer to list_t
+ * @n: conts char string
+ * Description: Add new node at the beginning of a listint_t list
+ * Return: Return new node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (new_node);
+	}
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
+/**
  * insert_nodeint_at_index - insert_nodeint_at_index
  * @head: pointer from head listint
  * @idx: int index
@@ -33,5 +55,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		curr = curr->next;
 		cont++;
 	}
+
+	if (idx == 0)
+	{
+		return (add_nodeint(head, n));
+	}
+
 	return (NULL);
 }
