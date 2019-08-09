4ae26c58f1b59f65644bd6ec340d275c143a3cff
luiseduardiazc <luis.diaz.car@gmail.com>
2019-08-09 16:10:56 +0000

    change code to be more efficient for long numbers

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,13 +8,20 @@
 void print_binary(unsigned long int n)
 {
 	int i = 0;
+	unsigned long int x = n;
 
 	if (n == 0)
 		_putchar('0');
 
-	while (n >> i)
+	while (x != 0)
+	{
+		/**
+		* This operation reduces length
+		* of every sequence of 1s by one.
+		*/
+		x = x >> 1;
 		i++;
-
+	}
 	for (i = i - 1; i >= 0; i--)
 	{
 		unsigned long int var = n >> i;
