4cf569670ceac783bfe7fd6b89ad721d3fdec973
luiseduardiazc <luis.diaz.car@gmail.com>
2019-08-13 21:59:51 +0000

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,52 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ *_strlen - reset number
+ *Description: This function return a length for some string
+ *@s: pointer char
+ *Return: int length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ *create_file - read_textfile
+ *@filename: name file to be open
+ *@text_content: content to write
+ *Description: this function create file
+ *Return: 1 if success or  -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int is_open, is_success;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+	is_open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (is_open == -1)
+	{
+		return (-1);
+	}
+	if (!text_content)
+	{
+		write(STDIN_FILENO, "", 1);
+		return (1);
+	}
+	is_success = write(STDIN_FILENO, text_content, _strlen(text_content) + 1);
+	if (is_success == -1)
+		return (-1);
+
+	return (1);
+}
