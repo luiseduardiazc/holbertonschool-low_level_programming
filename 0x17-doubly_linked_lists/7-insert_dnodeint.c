f905897645c5d53504fbb77b2d28cf6dd0596e29
luiseduardiazc <luis.diaz.car@gmail.com>
2019-09-15 23:28:11 +0000

    add new validation

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,7 +39,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
-	if (idx == 0)
+	if (idx == 0 && current_node != NULL)
 		add_dnodeint(h, n);
 		return (new_node);
 
