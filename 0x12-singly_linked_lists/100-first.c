6a998622556b04feefbd9d36475ddc151f7d4653
luiseduardiazc <luis.diaz.car@gmail.com>
2019-09-27 23:18:18 +0000

    change malloc by printf

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,10 +10,6 @@ void print_before_main(void) __attribute__ ((constructor));
 */
 void print_before_main(void)
 {
-	char *str_complete = NULL;
-
-	str_complete = malloc(sizeof(char) * 98);
-	strcpy(str_complete, "You're beat! and yet, you must allow,\n");
-	strcat(str_complete, "I bore my house upon my back!\n");
-	printf("%s", str_complete);
+	printf("%s", "You're beat! and yet, you must allow,\n");
+	printf("%s", "I bore my house upon my back!\n");
 }
