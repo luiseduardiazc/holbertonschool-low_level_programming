71e73f66820ca12036555f11f2e4d42f75f4b505
luiseduardiazc <luis.diaz.car@gmail.com>
2019-09-27 23:13:43 +0000

    change calloc by malloc

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -12,9 +12,8 @@ void print_before_main(void)
 {
 	char *str_complete = NULL;
 
-	str_complete =  calloc(98, sizeof(char));
+	str_complete = malloc(sizeof(char) * 98);
 	strcpy(str_complete, "You're beat! and yet, you must allow,\n");
 	strcat(str_complete, "I bore my house upon my back!\n");
 	printf("%s", str_complete);
-	free(str_complete);
 }
