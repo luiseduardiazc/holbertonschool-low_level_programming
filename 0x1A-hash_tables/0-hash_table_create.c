e7b34d29cce6ab63d7c6c69360d15028201f3d4c
luiseduardiazc <luis.diaz.car@gmail.com>
2019-10-10 23:24:15 +0000

    fixed betty task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,13 +1,22 @@
 #include "hash_tables.h"
+/**
+ * hash_table_create - hash_table_create
+ *@size: size of array
+ *Return: new_hash_table type hash_table_t
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table = NULL;
+
+	if (size == 0)
+		return (NULL);
+
 	new_hash_table = malloc(sizeof(hash_node_t));
 	if (new_hash_table == NULL)
 	{
 		return  (NULL);
 	}
-	new_hash_table->array = malloc(sizeof(int) * size);
+	new_hash_table->array = malloc(sizeof(unsigned long int) * size);
 	if (new_hash_table->array == NULL)
 	{
 		return (NULL);
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -33,3 +33,4 @@ typedef struct hash_table_s
 } hash_table_t;
 hash_table_t *hash_table_create(unsigned long int size);
 #endif
+
