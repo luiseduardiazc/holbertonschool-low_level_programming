cffe653c084a41541cbac246a6e3bbf95fc070e6
luiseduardiazc <luis.diaz.car@gmail.com>
2019-10-11 22:08:48 +0000

    change malloc by calloc task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		return  (NULL);
 	}
-	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	/* new_hash_table->array = malloc(sizeof(hash_node_t *) * size); */
+	new_hash_table->array = calloc(size, sizeof(hash_node_t *));
 	if (new_hash_table->array == NULL)
 	{
 		return (NULL);
