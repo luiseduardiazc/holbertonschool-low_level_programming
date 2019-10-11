1504e6eff9c4b64981376486d074a6409cda7c77
luiseduardiazc <luis.diaz.car@gmail.com>
2019-10-11 18:32:06 +0000

    use strdup to asing key and value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,28 +16,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (strlen(key) == 0)
 		return (0);
-	if (ht->size == 0)
-		return (0);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
 
-	new_node->key = (char *)key;
-	new_node->value = (char *)value;
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 	new_node->next = NULL;
 	index = key_index((const unsigned char *)key, ht->size);
 
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = new_node;
-		return (1);
-	}
 	existing_node = ht->array[index];
 	while (existing_node != NULL)
 	{
 		if (strcmp(existing_node->key, key) == 0)
 		{
-			existing_node->value = (char *)value;
+			free(existing_node->value);
+			existing_node->value = strdup(value);
 			return (1);
 		}
 		existing_node = existing_node->next;
