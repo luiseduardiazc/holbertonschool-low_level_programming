80c8daa6f2137f76eac591ee597508339c3c258c
luiseduardiazc <luis.diaz.car@gmail.com>
2019-10-11 18:17:40 +0000

    fixed return

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -38,7 +38,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(existing_node->key, key) == 0)
 		{
 			existing_node->value = (char *)value;
-			return (0);
+			return (1);
 		}
 		existing_node = existing_node->next;
 	}
