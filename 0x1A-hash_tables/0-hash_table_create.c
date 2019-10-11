9f8a22f5a1199e197dfd8f09e5c14a4366d35a60
luiseduardiazc <luis.diaz.car@gmail.com>
2019-10-11 17:43:11 +0000

    fixed amount memory

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,12 +11,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size < 1)
 		return (NULL);
 
-	new_hash_table = malloc(sizeof(hash_node_t));
+	new_hash_table = malloc(sizeof(hash_table_t));
 	if (new_hash_table == NULL)
 	{
 		return  (NULL);
 	}
-	new_hash_table->array = calloc(size, sizeof(hash_node_t *));
+	new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_hash_table->array == NULL)
 	{
 		return (NULL);
