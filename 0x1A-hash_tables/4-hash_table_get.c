576aecb686fbf8349383f97a9bc049b4f0116728
luiseduardiazc <luis.diaz.car@gmail.com>
2019-10-11 18:38:29 +0000

    add task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - This function get element by key
+ * @ht: hash table
+ * @key: key
+ * Return: value or NULL 
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	hash_node_t *head = NULL;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	if (strlen(key) == 0)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[index];
+	if (head == NULL)
+		return (NULL);
+	while (head != NULL)
+	{
+		if (strcmp(head->key, key) == 0)
+		{
+			return (head->value);
+		}
+		head = head->next;	
+	}
+	return (NULL);
+}
