b00f645a8a51b21fde9caefb06c4c4486e01b848
luiseduardiazc <luis.diaz.car@gmail.com>
2019-10-11 00:12:20 +0000

    fixed betty style task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,9 @@
+/**
+* hash_djb2 - hash_djb2
+* Description: This function create a hahs code
+* @str: parameter type  string
+* Return: hash code
+*/
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
