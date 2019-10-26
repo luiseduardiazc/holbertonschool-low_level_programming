8513a8b683e939a83b06015109ec06eba542fd9b
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-10-26 11:30:36 -0500

    fixed betty

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,13 +1,22 @@
 #include "sort.h"
+
+/**
+ * bubble_sort - bubble_sort
+ * @array: array
+ * @size: size
+ * Return: Nothing
+ */
 void bubble_sort(int *array, size_t size)
 {
 	int i;
 	int j;
 	int temp;
+
 	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j < size - i; j++) {
-			if(array[j] > array [j + 1])
+		for (j = 0; j < size - i; j++)
+		{
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
