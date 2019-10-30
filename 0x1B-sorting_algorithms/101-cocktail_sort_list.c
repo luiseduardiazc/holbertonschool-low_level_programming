a4d5d0b36e7c2c67b08780c62c52aefb996733c2
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-10-30 16:30:25 -0500

    add new solution

--- 0x1B-sorting_algorithms/101-cocktail_sort_list.c
+++ 0x1B-sorting_algorithms/101-cocktail_sort_list.c
@@ -36,37 +36,40 @@ void cocktail_sort_list(listint_t **list)
 	int swap;
 	listint_t *last = NULL, *head = *list;
 
-	if (!list || !head || !(*list)->next)
-		return;
-	do {
-		swap = 0;
-		while (head)
-		{
-			if (head->next != NULL && (head->n > head->next->n))
+	if (list)
+	{
+		if (!list || !(*list) || !head || !(*list)->next)
+			return;
+		do {
+			swap = 0;
+			while (head)
 			{
-				swap_list(head, head->next, list);
-				head = head->prev;
-				swap = 1;
-				continue;
+				if (head->next != NULL && (head->n > head->next->n))
+				{
+					swap_list(head, head->next, list);
+					head = head->prev;
+					swap = 1;
+					continue;
+				}
+				if (head->next == NULL)
+					last = head;
+				head = head->next;
 			}
-			if (head->next == NULL)
-				last = head;
-			head = head->next;
-		}
-		if (!swap)
-			break;
-		while (last)
-		{
-			if (last->prev && (last->n < last->prev->n))
+			if (!swap)
+				break;
+			while (last)
 			{
-				swap_list(last->prev, last, list);
-				last = last->next;
-				swap = 1;
-				continue;
+				if (last->prev && (last->n < last->prev->n))
+				{
+					swap_list(last->prev, last, list);
+					last = last->next;
+					swap = 1;
+					continue;
+				}
+				if (last->prev == NULL)
+					head = last;
+				last = last->prev;
 			}
-			if (last->prev == NULL)
-				head = last;
-			last = last->prev;
-		}
-	} while (swap);
+		} while (swap);
+	}
 }
