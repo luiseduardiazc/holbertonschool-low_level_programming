64f1a6dc82055c952e67a4e3c06cf256ce52b969
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-12-02 15:50:55 -0500

    init project

---
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - binary_tree_insert_left
+ * @parent: root
+ * @value: value to be insert
+ * @return: tree
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node = NULL;
+
+    new_node = malloc(sizeof(binary_tree_t));
+    if (!new_node)
+        return (NULL);
+    new_node->n = value;
+    new_node->parent = NULL;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    if (!parent)
+    {
+        return (new_node);
+    }
+    return (NULL);
+}
