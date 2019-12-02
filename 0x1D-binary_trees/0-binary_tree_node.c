02e9288d8a3ded47e1c9ed17afa656ddc31533a6
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-12-02 16:21:22 -0500

    fixed betty task 0

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -1,27 +1,27 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - binary_tree_insert_left
+ * binary_tree_node - binary_tree_node
  * @parent: root
  * @value: value to be insert
- * @return: new binary_tree_node
+ * Return: new binary_tree_node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node = NULL;
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (!new_node)
-        return (NULL);
-    new_node->n = value;
-    new_node->parent = NULL;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    if (!parent)
-    {
-        return (new_node);
-    }
-    new_node->parent = parent;
+	new_node = malloc(sizeof(binary_tree_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = value;
+	new_node->parent = NULL;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	if (!parent)
+	{
+		return (new_node);
+	}
+	new_node->parent = parent;
 
-    return (new_node);
+	return (new_node);
 }
--- 0x1D-binary_trees/binary_trees.h
+++ 0x1D-binary_trees/binary_trees.h
@@ -1,8 +1,10 @@
 #ifndef BINARY_TREES_BINARY_TREES_H
 #define BINARY_TREES_BINARY_TREES_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -13,16 +15,19 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+
 void binary_tree_print(const binary_tree_t *);
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
 #endif
