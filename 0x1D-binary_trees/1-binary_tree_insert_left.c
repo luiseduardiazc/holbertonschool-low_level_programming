518ed371df8a61aabf99dfe17f39a1cb6e5da8e4
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-12-04 18:15:19 -0500

    refactor task 1

--- 0x1D-binary_trees/1-binary_tree_insert_left.c
+++ 0x1D-binary_trees/1-binary_tree_insert_left.c
@@ -9,33 +9,23 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
-	binary_tree_t *current = parent;
+	binary_tree_t *aux = parent;
 
 	new_node = binary_tree_node(NULL, value);
 	if (!parent || !new_node)
 		return (NULL);
 
-	while (current)
+	if (!parent->left)
 	{
-		if (!current->left)
-		{
-			/**
-			 * If parent already has a left-child, the new node must take its place,
-			 * and the old left-child must be set as the left-child of the new node
-			 */
-			if (current->parent->left->n == current->n)
-			{
-				new_node->parent = current->parent;
-				current->parent->left = new_node;
-				new_node->left = current;
-				new_node->left->parent = new_node;
-				return (new_node);
-			}
-			new_node->parent = current;
-			current->left = new_node;
-			break;
-		}
-		current = current->left;
+		parent->left = binary_tree_node(parent, value);
+		return (parent);
 	}
-	return (current);
+
+	aux = parent->left;
+	new_node = (binary_tree_node(parent, value));
+	new_node->left = aux;
+	aux->parent = new_node;
+	parent->left = new_node;
+
+	return (new_node);
 }
