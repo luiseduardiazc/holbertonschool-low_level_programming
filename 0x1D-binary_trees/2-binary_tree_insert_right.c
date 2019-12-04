671b2ee58370418599e6fcebd65e2e74c1ec9861
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-12-04 18:20:30 -0500

    refactor task 2

--- 0x1D-binary_trees/2-binary_tree_insert_right.c
+++ 0x1D-binary_trees/2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - binary_tree_insert_left
+ * binary_tree_insert_right - binary_tree_insert_right
  * @parent: root
  * @value: value to be insert
  * Return: new binary_tree_node
@@ -9,33 +9,22 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
-	binary_tree_t *current = parent;
+	binary_tree_t *aux = parent;
 
 	new_node = binary_tree_node(NULL, value);
 	if (!parent || !new_node)
 		return (NULL);
-
-	while (current)
+	if (!parent->right)
 	{
-		if (!current->right)
-		{
-			/**
-			 * If parent already has a right-child, the new node must take its place,
-			 * and the old ight-child must be set as the right-child of the new node
-			 */
-			if (current->parent->right->n == current->n)
-			{
-				new_node->parent = current->parent;
-				current->parent->right = new_node;
-				new_node->right = current;
-				new_node->right->parent = new_node;
-				return (new_node);
-			}
-			new_node->parent = current;
-			current->right = new_node;
-			break;
-		}
-		current = current->right;
+		parent->right = binary_tree_node(parent, value);
+		return (parent);
 	}
-	return (current);
+
+	aux = parent->right;
+	new_node = (binary_tree_node(parent, value));
+	new_node->right = aux;
+	aux->parent = new_node;
+	parent->right = new_node;
+
+	return (new_node);
 }
