2b54e00742b222c0f56231b7503387abaf41db43
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-12-04 21:06:01 -0500

    add task 14-binary_tree_balance.c

---
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: Pointer to the node to measures the balance
+ * Return: the balance factor of a binary tree
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t height_left;
+	size_t height_right;
+
+	if (!tree)
+		return (0);
+	height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_left - height_right);
+}
