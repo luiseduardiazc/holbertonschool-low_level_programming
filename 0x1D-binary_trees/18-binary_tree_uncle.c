6a6d6dc55b31b6ef212403d8da9b302ede2426a3
Luis Eduardo D <luis.diaz.car@gmail.com>
2019-12-06 00:10:59 -0500

    add task 18

---
+++ 0x1D-binary_trees/18-binary_tree_uncle.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - binary_tree_sibling
+ * @node: node is a pointer to the node to find the sibling
+ * Return: return a pointer to the sibling node
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *sibling = NULL;
+
+	if (!node)
+		return (NULL);
+	if (!node->parent)
+		return (NULL);
+	if (!node->parent->left || !node->parent->right)
+		return (NULL);
+	if (node->parent->left == node)
+		sibling = node->parent->right;
+	if (node->parent->right == node)
+		sibling = node->parent->left;
+	return (sibling);
+}
+
+/**
+ * binary_tree_uncle - binary_tree_uncle
+ * @node: is a pointer to the node to find the uncle
+ * Return: return a pointer to the uncle node
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+	if (!node->parent)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
+}
