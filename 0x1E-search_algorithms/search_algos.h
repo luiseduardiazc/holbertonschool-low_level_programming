5726cf46681ec74530cb3fe09218e69e2990eeb9
Luis Eduardo D <luis.diaz.car@gmail.com>
2020-01-15 17:10:36 -0500

    add search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,6 @@
-//
-// Created by luis on 2020-01-15.
-//
-
-#ifndef INC_0X1E_SEARCH_ALGORITHMS_SEARCH_ALGOS_H
-#define INC_0X1E_SEARCH_ALGORITHMS_SEARCH_ALGOS_H
-
-#endif //INC_0X1E_SEARCH_ALGORITHMS_SEARCH_ALGOS_H
+#ifndef SEARCH_ALGORITHMS_H
+#define SEARCH_ALGORITHMS_H
+#include <stdio.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
