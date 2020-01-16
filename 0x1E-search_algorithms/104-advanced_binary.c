496ffa5349e0e763e6a79ecf4b194390b0730869
luiseduardiazc <luis.diaz.car@gmail.com>
2020-01-16 01:41:02 +0000

    send original size

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -8,15 +8,18 @@
  * @array: The array to be printed
  * @low: low index
  * @high: high index
+ * @size: original size of array
  * Return: Nothing
  */
-void print_array(int *array, int low, int high)
+void print_array(int *array, int low, int high, int size)
 {
 	int i;
 
 	i = low;
+	if (high == size)
+		high = high - 1;
 	printf("Searching in array: ");
-	while (array && i < high)
+	while (array && i <= high)
 	{
 
 		if (i > low)
@@ -33,21 +36,24 @@ void print_array(int *array, int low, int high)
  * @low: low index
  * @high: high index
  * @value: value to find
+ * @size: original size of array
  * Return: int
  *
  */
-int search(int *array, int low, int high, int value)
+int search(int *array, int low, int high, int value, int size)
 {
 	int mid;
 
 	if ((low >= high) || (array == NULL))
 		return (-1);
-	print_array(array, low, high);
+	print_array(array, low, high, size);
 	mid = (low + high) / 2;
+	if (value == array[mid])
+		return (mid);
 	if (value > array[mid])
-		return (search(array, mid + 1, high, value));
+		return (search(array, mid + 1, high, value, size));
 	if (value < array[mid])
-		return (search(array, low, mid - 1, value));
+		return (search(array, low, mid - 1, value, size));
 
 	return (mid);
 }
@@ -61,5 +67,5 @@ int search(int *array, int low, int high, int value)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	return (search(array, 0, size, value));
+	return (search(array, 0, size, value, size));
 }
